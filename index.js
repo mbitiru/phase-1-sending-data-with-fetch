766fb110b0af313ece28210bb2fbcc69ed7e2e27
tanveer.chege@student.moringaschool.com <tanveer.chege@student.moringaschool.com>
2022-08-31 08:50:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+//function submitData which is the name and email and return fetch()
+function submitData( name, email ) {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email,
+      } )
+    } )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then(formData => {
+        let id = formData["id"]
+        let body = document.querySelector("body")
+        body.innerHTML = id;
+    } )
+    .catch( function ( error ) {
+        alert("Unauthorised access")
+      document.body.innerHTML = error.message
+    } );
+}
+
+ssssssssssssssssssssssssssssssssssssssssssss
